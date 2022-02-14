c5d59f56c6b38507bdb2ce369de2d8b0d5e36adf
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-02-14 09:51:58 +0000

    Add behaviour to Navigation component.

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -1,11 +1,38 @@
+import React from "react";
+import "./Navigation.css";
+
 const Navigation = () => {
+  const [activePane, setActivePane] = React.useState("search");
+
+  const handleClick = (name) => {
+    setActivePane(name);
+  };
+
   return (
     <div className="navigation">
       <div>
-        <button className="active">Rechercher un livre</button>
-        <button>Encoder un livre</button>
+        <NavigationButton
+          active={activePane === "search"}
+          name="Rechercher un livre"
+          onClick={() => handleClick("search")}
+        />
+        <NavigationButton
+          active={activePane === "add"}
+          name="Encoder un livre"
+          onClick={() => handleClick("add")}
+        />
       </div>
     </div>
+  );
+};
+
+const NavigationButton = ({ active, name, onClick }) => {
+  return active ? (
+    <button className="active" onClick={onClick}>
+      {name}
+    </button>
+  ) : (
+    <button onClick={onClick}>{name}</button>
   );
 };
 
