1599f7ba9db49804207b8477a66ca1fcdde3f051
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-02-14 09:32:45 +0000

    Extracted Navigation component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import Navigation from "./Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1 className="title">Ma bibliothèque</h1>
+      <Navigation />
+      <div className="content">
+        <form className="searchForm">
+          <input type="text" />
+          <button>Rechercher</button>
+        </form>
+        <table className="searchResults">
+          <thead>
+            <tr>
+              <th>Auteur</th>
+              <th>Titre</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>J.R.R. Tolkien</td>
+              <td>La Communauté de l'Anneau</td>
+            </tr>
+            <tr>
+              <td>J.R.R. Tolkien</td>
+              <td>Les Deux Tours</td>
+            </tr>
+            <tr>
+              <td>J.R.R. Tolkien</td>
+              <td>Le Retour du Roi</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/Navigation.js
@@ -0,0 +1,12 @@
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div>
+        <button className="active">Rechercher un livre</button>
+        <button>Encoder un livre</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
