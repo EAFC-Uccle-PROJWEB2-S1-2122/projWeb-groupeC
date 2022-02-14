31a8d3b0eb14c8d688f9232df1e3da62a3531530
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-02-14 11:04:29 +0000

    Draw add form.

--- client/src/AddPane.js
+++ client/src/AddPane.js
@@ -1,5 +1,21 @@
+import "./AddPane.css";
+
 const AddPane = () => {
-  return <p>Encoder un livre.</p>;
+  return (
+    <form className="addForm">
+      <div>
+        <label htmlFor="authorInput">Auteur</label>
+        <input type="text" id="authorInput" />
+      </div>
+      <div>
+        <label htmlFor="titleInput">Title</label>
+        <input type="text" id="titleInput" />
+      </div>
+      <div>
+        <button type="submit">Ajouter</button>
+      </div>
+    </form>
+  );
 };
 
 export default AddPane;
