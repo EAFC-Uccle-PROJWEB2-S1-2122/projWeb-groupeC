8ba0b5e6aa63e4a9d87649ee10f5620c6c4202d0
Yassine182 <50362825+YassineOslm@users.noreply.github.com>
2022-03-13 12:48:49 +0000

    Page Blanche React.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,7 @@
-import React from "react";
-import AddPane from "./AddPane";
-import Navigation from "./Navigation";
-import SearchPane from "./SearchPane";
 import "./App.css";
 
-function App() {
-  const [activePane, setActivePane] = React.useState("search");
-
-  const handleClick = (name) => {
-    setActivePane(name);
-  };
-
-  return (
-    <>
-      <h1 className="title">Ma bibliothèque</h1>
-      <Navigation activePane={activePane} onClick={handleClick} />
-      <div className="content">
-        {activePane === "search" ? <SearchPane /> : <AddPane />}
-      </div>
-    </>
-  );
+function App(){
+  return <div className = "App"></div>
 }
 
 export default App;
