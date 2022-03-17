83ed95cd96f3aaad2bfe249f30fd749d20bd7cec
Yassine182 <50362825+YassineOslm@users.noreply.github.com>
2022-03-17 21:05:00 +0000

    importing list of restaurants.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var logger = require("morgan");
 const cors = require("cors");
 
 var apiRouter = require("./routes/api");
+var htmlRouter = require("./routes/index.html");
 
 var app = express();
 
@@ -14,6 +15,7 @@ app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 
-app.use("/api", apiRouter);
+//app.use("/api", apiRouter);
+app.use("/index.html", htmlRouter);
 
 module.exports = app;
